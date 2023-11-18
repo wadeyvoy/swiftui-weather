f1cfda43a53a2b18c5024df00e6c0ec296b92704
umucaxej <151387946+umucaxej@users.noreply.github.com>
2023-11-18 19:23:36 +0330

    Update Weather.swift

--- sample-weather/Model/Weather.swift
+++ sample-weather/Model/Weather.swift
@@ -18,6 +18,16 @@ struct WeatherResponse: Decodable{
     var weather: [WeatherModel]?
 }
 
+struct WeatherResponse: Decodable{
+    var main: Weather?
+    var weather: [WeatherModel]?
+}
+
+struct WeatherResponse: Decodable{
+    var main: Weather?
+    var weather: [WeatherModel]?
+}
+
 struct Weather: Decodable{
     var temp: Double?
 }
