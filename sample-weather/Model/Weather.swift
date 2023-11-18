fbb3eec5f4c3639f336a80c5e4160e9c8c08d472
umucaxej <151387946+umucaxej@users.noreply.github.com>
2023-11-18 19:23:26 +0330

    Update Weather.swift

--- sample-weather/Model/Weather.swift
+++ sample-weather/Model/Weather.swift
@@ -1,1 +1,45 @@
 
+//
+//  Weather.swift
+//  sample-weather
+//
+//  Created by saeid on 11/8/23.
+//
+
+import Foundation
+
+struct WeatherResponse: Decodable{
+    var main: Weather?
+    var weather: [WeatherModel]?
+}
+
+struct WeatherResponse: Decodable{
+    var main: Weather?
+    var weather: [WeatherModel]?
+}
+
+struct Weather: Decodable{
+    var temp: Double?
+}
+
+struct Weather: Decodable{
+    var temp: Double?
+}
+
+struct WeatherModel: Decodable{
+    var main: String?
+}
+
+
+struct WeatherResponse: Decodable{
+    var main: Weather?
+    var weather: [WeatherModel]?
+}
+
+struct Weather: Decodable{
+    var temp: Double?
+}
+
+struct WeatherModel: Decodable{
+    var main: String?
+}
