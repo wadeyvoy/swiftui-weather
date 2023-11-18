79d14efb59a2711db9be23a667787e8f8ef09978
wadeyvoy <151392987+wadeyvoy@users.noreply.github.com>
2023-11-18 20:45:44 +0330

    Update sample_weatherApp.swift

--- sample-weather/sample_weatherApp.swift
+++ sample-weather/sample_weatherApp.swift
@@ -1,1 +1,25 @@
+//
+//  sample_weatherApp.swift
+//  sample-weather
+//
+//  Created by saeid on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct sample_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+struct sample_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
 
